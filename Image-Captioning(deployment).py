be7ad4388d6a631a8a17d3051f4648e150730a31
SrijanChakraborty2003 <166740425+SrijanChakraborty2003@users.noreply.github.com>
2025-04-02 19:47:46 +0530

    Update Image-Captioning(deployment).py

--- Image-Captioning(deployment).py
+++ Image-Captioning(deployment).py
@@ -36,7 +36,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image")
     
     if st.button("Generate Caption"):
         caption = generate_caption(image)
